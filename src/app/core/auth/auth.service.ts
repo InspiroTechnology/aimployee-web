4f95540d51d9b65067106485ca62e60d492a4513
Simon Shen <shentiexiong@gmail.com>
2025-03-23 21:50:21 +1300

    Update ApiService base URL and enhance AuthService with login method using ApiService

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service'; // Import ApiService
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private apiService: ApiService) {} // Inject ApiService
 
-  constructor() { }
+  login(userAccounts: string, password: string): Observable<any> {
+    console.log('login');
+    console.log(userAccounts);
+    console.log(password);
+    
+    
+    
+    return this.apiService.post('/app/user/login', { userAccounts, password });
+  }
 }
