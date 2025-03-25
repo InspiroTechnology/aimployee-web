0e5e41e330a03b5614779a4d7e8541ce4614b082
Simon Shen <shentiexiong@gmail.com>
2025-03-25 13:47:46 +1300

    Update chat component to render HTML content and modify query for chat streaming

--- src/app/features/chat/new-flow-chat/new-flow-chat.component.ts
+++ src/app/features/chat/new-flow-chat/new-flow-chat.component.ts
@@ -19,7 +19,7 @@ export class NewFlowChatComponent implements OnInit {
   ngOnInit(): void {
     const apiUrl = 'http://1.14.73.74:8081/app/api/v1/llm/chat/messages'; // 替换成真实地址
     const apiKey = 'e05ff68818ab4d2a8a28a01662cb8290';
-    const payload = { responseMode: 'streaming', query: '你好，你是谁,今天天气如何？' ,
+    const payload = { responseMode: 'streaming', query: '怎么评价新西兰？' ,
     };
 
     this.chatService.streamChat(apiUrl, payload, apiKey).subscribe({
