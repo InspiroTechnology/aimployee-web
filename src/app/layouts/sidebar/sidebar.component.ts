6ec42ece0cc31177e1920f1e92ed9b7874372772
Simon Shen <shentiexiong@gmail.com>
2025-03-26 10:25:23 +1300

    Refactor sidebar navigation to use routerLink for improved routing and add RouterModule to SidebarComponent imports

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { LayOutService } from '../../services/lay-out.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list'; // Import MatListModule
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router'; // Import RouterModule
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatListModule],
+  imports: [MatIconModule, MatButtonModule, MatListModule, RouterModule], // Add RouterModule here
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'], // Fixed typo here
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
   constructor(public layoutService: LayOutService) {}
