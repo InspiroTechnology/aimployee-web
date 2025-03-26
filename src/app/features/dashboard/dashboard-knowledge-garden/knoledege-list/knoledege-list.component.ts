baf8d0da53eaaadcc47f2bff5f2e8746c767bc9e
Simon Shen <shentiexiong@gmail.com>
2025-03-26 15:58:04 +1300

    Enhance KnoledegeListComponent to manage tag selection changes and unsubscribe on destroy

--- src/app/features/dashboard/dashboard-knowledge-garden/knoledege-list/knoledege-list.component.ts
+++ src/app/features/dashboard/dashboard-knowledge-garden/knoledege-list/knoledege-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KnowledgeGardenService } from '../../../../services/knowledge-garden.service';
 import { TagService } from '../../../../services/tag.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-knoledege-list',
@@ -10,28 +11,40 @@ import { TagService } from '../../../../services/tag.service';
   templateUrl: './knoledege-list.component.html',
   styleUrls: ['./knoledege-list.component.scss'],
 })
-export class KnoledegeListComponent implements OnInit {
+export class KnoledegeListComponent implements OnInit, OnDestroy {
   knowledgeData: any[] = [];
   currentPage: number = 1;
   pageSize: number = 10;
   totalRecords: number = 0;
   Math = Math; // Expose Math object for use in the template
   Array = Array; // Expose Array object for use in the template
+  private tagSubscription: Subscription | null = null;
 
-  constructor(private knowledgeGardenService: KnowledgeGardenService,public tagService:TagService) {}
+  constructor(
+    private knowledgeGardenService: KnowledgeGardenService,
+    public tagService: TagService
+  ) {}
 
   ngOnInit(): void {
     this.loadKnowledge();
+    this.tagSubscription = this.tagService.selectedTagChanged.subscribe(() => {
+      this.loadKnowledge();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.tagSubscription) {
+      this.tagSubscription.unsubscribe();
+    }
   }
 
   loadKnowledge(): void {
- // Get tags from TagService and filter out null values
- const tags = this.tagService.getSelectedTag()
- ? [this.tagService.getSelectedTag()]
- : [];
-const filteredTags = tags.filter((tag): tag is string => tag !== null);
+    // Get tags from TagService and filter out null values
+    const tags = this.tagService.getSelectedTag()
+      ? [this.tagService.getSelectedTag()]
+      : [];
+    const filteredTags = tags.filter((tag): tag is string => tag !== null);
 
-    
     this.knowledgeGardenService
       .getPaginatedKnowledge(this.currentPage, this.pageSize, filteredTags)
       .subscribe({
